ad25241e746eda31415eebb05ec85aabfeb0c8d2
Josh Smeaton <josh.smeaton@gmail.com>
2019-04-13 22:55:35 +1000

    Added a test runner script

--- {{cookiecutter.directory_name}}/tests/test_{{cookiecutter.package_name}}.py
+++ {{cookiecutter.directory_name}}/tests/test_{{cookiecutter.package_name}}.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django.test import TestCase
+
 from {{cookiecutter.package_name}} import __version__
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+class {{cookiecutter.package_name|title}}TestCase(TestCase):
+    def test_version(self):
+        self.assertEqual(__version__, "0.1.0")
